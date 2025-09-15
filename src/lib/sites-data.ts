f50276692ce1b654dfe0d6f6a9f7009adb9e5055
Isham Singh <ishamsingh1310@gmail.com>
2025-09-15 10:41:45 +0000

    remove the video unavailable part from website only keep the video link

--- src/lib/sites-data.ts
+++ src/lib/sites-data.ts
@@ -144,19 +144,14 @@ export const heritageSites: HeritageSite[] = [
     historicalContext:
       "The fort is quite ancient. Remnants of Microlithic man have been discovered here. The various Puranas (ancient scriptures) like Matsyapurana, Agnipurana and Skandapurana include many references about Harishchandragad. Its origin is said to have been in the 6th century, during the rule of Kalachuri dynasty. The citadel was built during this era. The various caves probably have been carved out in the 11th century. In these caves are idols of Lord Vishnu. Though the cliffs are named Taramati and Rohidas, they are not related to Ayodhya. Great sage Changdev (one who created the epic Tatvasaar), used to meditate here in the 14th century. The caves are from the same period. The various constructions on the fort and those existing the surrounding region point to the existence of diverse cultures here. The carvings on the temples of Nageshwar (in Khireshwar village), in the Harishchandreshwar temple and in the cave of Kedareshwar indicate that the fort belongs to the medieval period, since it is related to mahadeva as a totem of tribes Mahadev Koli. They were controlling the fort before Moguls. Later the fort was under the control of Moguls. The Marathas captured it in 1747.",
     architecturalSignificance:
-      'The fort features diverse cultural constructions. The carvings on the temples of Nageshwar, Harishchandreshwar, and the Kedareshwar cave indicate medieval architectural styles. The Temple of Harishchandreshwar is a marvelous example of ancient Indian stone carving art. This temple is about 16m high from its base. Around this temple there a few caves & ancient water tanks. The river Mangal Ganga is said to originate from one of the tanks located close to the temple. The top of the temple resembles construction with the north-Indian temples. A similar temple is situated in Buddha-Gaya. On the main entrance there are sculptures of faces. These are faces of guards of the temple. On the left side of the entrance is a Devnagri inscription, which is about saint Changdev.',
+      "The fort features diverse cultural constructions. The carvings on the temples of Nageshwar, Harishchandreshwar, and the Kedareshwar cave indicate medieval architectural styles. The Temple of Harishchandreshwar is a marvelous example of ancient Indian stone carving art. This temple is about 16m high from its base. Around this temple there a few caves & ancient water tanks. The river Mangal Ganga is said to originate from one of the tanks located close to the temple. The top of the temple resembles construction with the north-Indian temples. A similar temple is situated in Buddha-Gaya. On the main entrance there are sculptures of faces. These are faces of guards of the temple. On the left side of the entrance is a Devnagri inscription, which is about saint Changdev.",
     notableEvents: [
       "Origin in the 6th century under the Kalachuri dynasty.",
       "Meditation place for the great sage Changdev in the 14th century.",
       "Captured by the Marathas in 1747.",
     ],
     notableFigures: ["Changdev"],
-    youtubeVideoLinks: [
-      "https://www.youtube.com/watch?v=f2a9v2gVj1E",
-      "https://www.youtube.com/watch?v=0h_Bbe9aM4c",
-      "https://www.youtube.com/watch?v=mflZzQ7L3aQ",
-      "https://www.youtube.com/watch?v=u2DLbEqHyJw"
-    ],
+    youtubeVideoLinks: ["https://youtu.be/u2DLbEqHyJw?si=nOPL8Xo0dDzWElwk"],
     waypoints: [
       {
         title: "Kedareshwar Cave",
@@ -167,7 +162,7 @@ export const heritageSites: HeritageSite[] = [
       {
         title: "Kokan Kada (Konkan Cliff)",
         description:
-          'This cliff faces west and looks down upon the Konkan. It provides views of the surrounding region. The cliff has an overhang, but has been climbed many times. Sometimes a circular rainbow (the Brocken spectre phenomenon) can be seen from this point. It can be seen only when there is a bit of mist in the valley, and the sun is right behind the person facing the valley. One phenomenon that can be observed at this place is the vertical cloud burst, in which the clouds nearing the cliff get sucked into the pit fall area below and are thrown vertically into the sky reaching more than 50 feet (15 m), creating the impression of a wall that is rising straight from the edge of the cliff without entering the landmass area.',
+          "This cliff faces west and looks down upon the Konkan. It provides views of the surrounding region. The cliff has an overhang, but has been climbed many times. Sometimes a circular rainbow (the Brocken spectre phenomenon) can be seen from this point. It can be seen only when there is a bit of mist in the valley, and the sun is right behind the person facing the valley. One phenomenon that can be observed at this place is the vertical cloud burst, in which the clouds nearing the cliff get sucked into the pit fall area below and are thrown vertically into the sky reaching more than 50 feet (15 m), creating the impression of a wall that is rising straight from the edge of the cliff without entering the landmass area.",
         imageId: "harishchandragad-waypoint-2",
       },
       {
@@ -179,7 +174,7 @@ export const heritageSites: HeritageSite[] = [
       {
         title: "Temple of Harishchandreshwar",
         description:
-          'This temple is a marvelous example of the fine art of carving sculptures out of stones that prevailed in ancient India. It is about 16 m high from its base. Around this temple there are a few caves & ancient water tanks. The river Mangal Ganga is said to originate from one of the tanks located close to the temple. The top of the temple resembles construction with the north-Indian temples. A similar temple is situated in Buddha-Gaya. Here we can see many tombs, in which a typical construction is seen. These are built by well-finished arranging stones one on top of the other. There are three main caves near the temple. The cisterns near the temple provide drinking water. A short distance away, another temple called Kashitirtha is located. The fascinating thing about this temple is that it has been carved out from a single huge rock. There are entrances from all four sides. On the main entrance there are sculptures of faces. These are faces of guards of the temple. On the left side of the entrance is a Devnagri inscription, which is about saint Changdev.',
+          "This temple is a marvelous example of the fine art of carving sculptures out of stones that prevailed in ancient India. It is about 16 m high from its base. Around this temple there are a few caves & ancient water tanks. The river Mangal Ganga is said to originate from one of the tanks located close to the temple. The top of the temple resembles construction with the north-Indian temples. A similar temple is situated in Buddha-Gaya. Here we can see many tombs, in which a typical construction is seen. These are built by well-finished arranging stones one on top of the other. There are three main caves near the temple. The cisterns near the temple provide drinking water. A short distance away, another temple called Kashitirtha is located. The fascinating thing about this temple is that it has been carved out from a single huge rock. There are entrances from all four sides. On the main entrance there are sculptures of faces. These are faces of guards of the temple. On the left side of the entrance is a Devnagri inscription, which is about saint Changdev.",
         imageId: "harishchandragad-waypoint-4",
       },
     ],
