0360ba61979c78ef5d47c28281d96c9987b0f3a1
Isham Singh <ishamsingh1310@gmail.com>
2025-09-19 13:44:39 +0000

    https://media.tripinvites.com/places/netarhat/upper-ghaghri-waterfalls/u

--- next.config.ts
+++ next.config.ts
@@ -76,6 +76,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.tripinvites.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
